9df5748c5bd79b03b235b0f7d09847f5c484244b
Zoe Hayes <zoehayes286@gmail.com>
2024-12-17 23:08:36 +0000

    Add 2024 Day 13 Solutions

---
+++ 2024/Day13/ClawContraptionPt1.py
@@ -0,0 +1,39 @@
+import os.path
+import itertools
+import functools
+import time
+import collections
+import functools
+import numpy as np
+import re
+
+filename = "input.txt"
+if not os.path.isfile(filename):
+    print('File does not exist.')
+else:
+    with open(filename) as f:
+        lines = f.read().splitlines()
+    
+    totalCost = 0
+    prizeCount = 0
+    for lineNo in range(0,len(lines),4):
+        a = lines[lineNo]
+        b = lines[lineNo+1]
+        prize = lines[lineNo+2]
+        
+        ax, ay = re.findall("\\d+", a)
+        bx, by = re.findall("\\d+", b)
+        px, py = re.findall("\\d+", prize)
+        
+        left = np.array([[int(ax), int(bx)],[int(ay),int(by)]])
+        right = np.array([int(px), int(py)])
+        
+        aCount, bCount = np.linalg.solve(left,right)
+        if round(aCount,5).is_integer() and round(bCount,5).is_integer():
+            cost = aCount * 3 + bCount * 1
+            prizeCount += 1
+            totalCost += cost
+        
+    print(f"totalCost {totalCost}")
+    print(f"prizeCount {prizeCount}")
+        
